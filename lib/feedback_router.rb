b0d6fbb37801dfa172b024f9b89489ca4cf5a1eb
Elliott Young <elliott.a.young@gmail.com>
2015-08-14 09:44:07 -0500

    Add Working Regex for splitting URL

--- lib/feedback_router.rb
+++ lib/feedback_router.rb
@@ -2,18 +2,23 @@ require "feedback_router/version"
 
 module FeedbackRouter
   def self.send(feedback_params, application_name)
-    @destination = ENV['FEEDBACK_LOCATION']
+    @base_url, @controller_route = set_up_destination
     @params      = set_params(feedback_params, application_name)
     send_request
   end
 
   private
 
+  def set_up_destination(url)
+    matches = url.match(/(https?:\/\/[\w._:-]+)(.*)/)
+    base_url = matches[1], controller_route = matches[2]
+  end
+
   def set_params
     feedback_params['app_name'] = application_name
   end
 
   def send_request
-    Faraday.post @destination, @params
+    
   end
 end
