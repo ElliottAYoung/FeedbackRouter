2345bda3f62f4fc37ee3024b217212353552cdc6
Elliott Young <elliott.a.young@gmail.com>
2015-08-14 08:29:59 -0500

    Worked out all logic except HTTP

--- lib/feedback_router.rb
+++ lib/feedback_router.rb
@@ -1,8 +1,19 @@
 require "feedback_router/version"
 
 module FeedbackRouter
-  def self.send(feedback_object, application_name)
+  def self.send(feedback_params, application_name)
     @destination = ENV['FEEDBACK_LOCATION']
+    @params      = set_params(feedback_params, application_name)
+    send_request
+  end
+
+  private
+
+  def set_params
+    feedback_params['app_name'] = application_name
+  end
+
+  def send_request
     
   end
 end
